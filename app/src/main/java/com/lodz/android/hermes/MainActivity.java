ec181f3abee9646b1b60d3c3be77423a13b15c75
LZ9 <lz.mms@foxmail.com>
2018-05-24 09:35:09 +0800

    Initial commit

---
+++ app/src/main/java/com/lodz/android/hermes/MainActivity.java
@@ -0,0 +1,26 @@
+package com.lodz.android.hermes;
+
+import android.os.Bundle;
+
+import com.lodz.android.component.base.activity.AbsActivity;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AbsActivity {
+
+    @Override
+    protected int getAbsLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void findViews(Bundle savedInstanceState) {
+        ButterKnife.bind(this);
+    }
+
+
+
+
+
+
+}
